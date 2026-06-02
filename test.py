6f6ae7802a6f93e929378fab5f0483e042233c79
GrandKenzy <kencv3002003@gmail.com>
2026-06-01 21:48:33 -0600

    Versión media, no está completa y tiene bastantes incompletos pero aún compila. Falta arreglar el sound, fonts y adiciones de GBA

--- test.py
+++
@@ -1,7 +0,0 @@
-try:
-    import rif.compiler as C
-    C.Compiler(plugin='megadrive').build('examples/megadrive')
-    print('OK')
-except Exception as e:
-    import traceback
-    traceback.print_exc()
